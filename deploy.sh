a57f2926069bc377232c2571a0eab0467add34f0
adhamgatriff <adhamgatriff@gmail.com>
2019-06-04 16:12:54 -0400

    act

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- docker kill frontend  > /dev/null 2>&1
- docker rm frontend > /dev/null 2>&1
- docker-compose build
- docker-compose up -d
+ sudo docker kill frontend  > /dev/null 2>&1
+ sudo docker rm frontend > /dev/null 2>&1
+ sudo docker-compose build
+ sudo docker-compose up -d
